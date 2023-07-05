419fb1e1d145aa929445a410dcec572cb7c7a86c
Halí V <halivelazquez@outlook.com>
2023-07-04 21:30:32 -0600

    feat: update migrations and models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at', 3)->nullable();
             $table->string('password');
+            $table->string('subscription')->nullable();
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(3);
         });
     }
 
