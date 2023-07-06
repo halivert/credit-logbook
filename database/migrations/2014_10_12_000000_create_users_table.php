b3a21ffc34694c3ffa7b47372371881043bdf43a
Halí V <halivelazquez@outlook.com>
2023-07-05 22:23:41 -0600

    feat: add fortify

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at', 3)->nullable();
             $table->string('password');
+            $table->text('two_factor_secret')->nullable();
+            $table->text('two_factor_recovery_codes')->nullable();
+            $table->timestamp('two_factor_confirmed_at', 3)->nullable();
             $table->string('subscription')->nullable();
             $table->rememberToken();
             $table->timestamps(3);
