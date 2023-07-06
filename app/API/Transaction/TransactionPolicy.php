3bc2174ddc8b8a95c0de9c81d374ce725d3852c0
Halí V <halivelazquez@outlook.com>
2023-07-05 21:48:10 -0600

    feat: add transactions

---
+++ app/API/Transaction/TransactionPolicy.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\API\Transaction;
+
+use App\API\CreditCard\CreditCard;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+
+class TransactionPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): Response
+    {
+        return $user->exists ? $this->allow() : $this->denyAsNotFound();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Transaction $transaction): Response
+    {
+        return $transaction->user->is($user)
+            ? $this->allow()
+            : $this->denyAsNotFound();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user, CreditCard $creditCard): Response
+    {
+        return $creditCard->user->is($user)
+            ? $this->allow()
+            : $this->denyAsNotFound();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Transaction $transaction): Response
+    {
+        return $transaction->user->is($user)
+            ? $this->allow()
+            : $this->denyAsNotFound();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Transaction $transaction): Response
+    {
+        return $transaction->user->is($user)
+            ? $this->allow()
+            : $this->denyAsNotFound();
+    }
+}
