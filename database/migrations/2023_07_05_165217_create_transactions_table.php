9c9d39aabf336fa3bd97046e01984c56ce40a902
Halí V <halivelazquez@outlook.com>
2023-07-11 13:12:03 -0600

    fix: primary key for uuids

--- database/migrations/2023_07_05_165217_create_transactions_table.php
+++ database/migrations/2023_07_05_165217_create_transactions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('transactions', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->foreignUuid('credit_card_id')->constrained();
             $table->string('concept');
             $table->dateTime('datetime');
