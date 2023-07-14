74d9d425af0883571b6a8d55589ff03e96f4772c
Halí V <halivelazquez@outlook.com>
2023-07-13 21:33:16 -0600

    feat: add other labels

---
+++ lang/es_MX/labels.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => 'nombre',
+    'due_date' => 'fecha límite de pago',
+    'closing_date' => 'fecha de corte',
+    'interest_rate' => 'tasa de interés',
+    'limit' => 'limite de crédito',
+];
