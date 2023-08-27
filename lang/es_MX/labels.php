c07a0b3e927b157e3088a0b1264366ad515db126
Halí V <halivelazquez@outlook.com>
2023-08-27 16:49:30 -0600

    chore: add translations

--- lang/es_MX/labels.php
+++ lang/es_MX/labels.php
@@ -6,4 +6,10 @@ return [
     'closing_date' => 'fecha de corte',
     'interest_rate' => 'tasa de interés',
     'limit' => 'limite de crédito',
+    'concept' => 'concepto',
+    'datetime' => 'fecha',
+    'amount' => 'cantidad',
+    'deadline_months' => 'meses para pagar',
+    'commission' => 'comisión',
+    'interest_rate' => 'tasa de interés',
 ];
