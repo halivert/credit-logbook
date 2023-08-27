6fc3a2c04a0340c9f49a96f36e1c4fb65fea2e1d
Halí V <halivelazquez@outlook.com>
2023-08-27 16:49:56 -0600

    feat: add transactions relation

--- app/API/CreditCard/CreditCard.php
+++ app/API/CreditCard/CreditCard.php
@@ -2,12 +2,14 @@
 
 namespace App\API\CreditCard;
 
+use App\API\Transaction\Transaction;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreditCard extends Model
@@ -32,6 +34,11 @@ class CreditCard extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
     }
 
     /**
