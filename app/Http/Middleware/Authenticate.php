dbfc31dd8eeae2c33e0079ab196f0b059c55fcd6
Halí V <halivelazquez@outlook.com>
2023-08-26 23:50:29 -0600

    feat: return 404 when guest

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,14 +4,20 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo(Request $request): void
     {
-        return $request->expectsJson() ? null : route('login');
+        abort(Response::HTTP_NOT_FOUND);
+    }
+
+    protected function unauthenticated($request, array $guards): void
+    {
+        abort(Response::HTTP_NOT_FOUND);
     }
 }
